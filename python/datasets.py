a5eecc8c794780b61ae32083fc679f3ca618ab65
darrelrobinson <dj22@gmx.com>
2020-10-22 14:50:02 +0200

    first commit

---
+++ python/datasets.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+olympic_running = pd.read_csv('../data/olympic_running.csv')
+PBS = pd.read_csv('../data/PBS.csv')
+ansett = pd.read_csv('../data/ansett.csv')
+vic_elec = pd.read_csv('../data/vic_elec.csv')
+tourism = pd.read_csv('../data/tourism.csv')
+aus_production = pd.read_csv('../data/aus_production.csv')
+global_economy = pd.read_csv('../data/global_economy.csv')
+aus_retail = pd.read_csv('../data/aus_retail.csv')
+us_employment = pd.read_csv('../data/us_employment.csv')
